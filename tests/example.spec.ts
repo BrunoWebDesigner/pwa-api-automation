55128173fe76d43eaafe91f086e66db341d16b87
Bruno Peres <brunowebdeveloper33@gmail.com>
2025-08-09 01:04:46 +0300

    Article Update - Put method

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -19,7 +19,6 @@ test('Get All Articles', async ({ request }) => {
   expect(articlesResponse.status()).toBe(200);
   expect(articlesResponseJSON.articles.length).toBeGreaterThan(0);
   expect(articlesResponseJSON.articles.length).toBeLessThanOrEqual(10);
-  expect(articlesResponseJSON.articlesCount).toEqual(10);
   console.log(articlesResponseJSON)
 });
 
@@ -69,3 +68,69 @@ test('Create and Delete Article', async ({ request }) => {
   expect(deleteArticleResponse.status()).toBe(204);
 });
 
+// How to create a post request test with authentication
+test('Create, Update and Delete Article', async ({ request }) => {
+  const tokenResponse = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data: {"user":{"email":"brunowebdeveloper33@gmail.com","password":"bondar27*"}}
+  });
+  const tokenResponseJSON = await tokenResponse.json();
+  const authToken = tokenResponseJSON.user.token;
+
+  const newArticleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles', {
+    data: {
+      "article": {
+          "title": "Test New Article",
+          "description": "Test Description 2",
+          "body": "Test body",
+          "tagList": []
+      }
+    },
+    headers: {
+      'Authorization': `Token ${authToken}`
+    }
+  })
+  const newArticleResponseJSON = await newArticleResponse.json();
+  expect(newArticleResponse.status()).toBe(201);
+  expect(newArticleResponseJSON.article.title).toEqual('Test New Article');
+  const slugId = newArticleResponseJSON.article.slug;
+
+  // Verify the article was created
+  const articlesResponse = await request.get('https://conduit-api.bondaracademy.com/api/articles', {
+    headers: {
+      'Authorization': `Token ${authToken}`
+    }
+  })
+  const articlesResponseJSON = await articlesResponse.json();
+  expect(articlesResponse.status()).toBe(200);
+  console.log('Article created successfully');
+  expect(articlesResponseJSON.articles[0].title).toEqual('Test New Article');
+
+  // Update the article
+  const updateArticleResponse = await request.put(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+    data: {
+      "article": {
+          "title": "Test New Article Updated",
+          "description": "Test Description Updated",
+          "body": "Test body Updated",
+          "tagList": []
+      }
+    },
+    headers: {
+      'Authorization': `Token ${authToken}`
+    }
+  });
+  const updateArticleResponseJSON = await updateArticleResponse.json();
+  const newSlugId = updateArticleResponseJSON.article.slug;
+  expect(updateArticleResponse.status()).toBe(200);
+  console.log('Article updated successfully');
+  expect(updateArticleResponseJSON.article.title).toEqual('Test New Article Updated');
+
+  // Delete the article
+  const deleteArticleResponse = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${newSlugId}`, {
+    headers: {
+      'Authorization': `Token ${authToken}`
+    }
+  });
+  expect(deleteArticleResponse.status()).toBe(204);
+  console.log('Article deleted successfully');
+});
