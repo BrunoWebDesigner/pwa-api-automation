94dfa16cd60a8bc3b7b2789a5902762b34891f32
Bruno Peres <brunowebdeveloper33@gmail.com>
2025-08-21 20:51:02 +0300

    feat: Create, Update and Delete Article new test structure

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -61,3 +61,53 @@ test('Create and Delete Article', async ({ api }) => {
         .getRequest(200)
     expect(articlesResponseTwo.articles[0].title).not.toEqual('Test 02');
 })
+
+test('Create, Update and Delete Article', async ({ api }) => {
+    // Create Article
+    const createArticleResponse = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .body({"article": {"title": "Test 03", "description": "Test Description 2", "body": "Test body", "tagList": []}})
+        .postRequest(201);
+    expect(createArticleResponse.article.title).toEqual('Test 03');
+    const slugId = createArticleResponse.article.slug;
+
+    // Verify Article Creation
+    const articlesResponse = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .params({limit:10, offset:0})
+        .getRequest(200)
+    expect(articlesResponse.articles[0].title).toEqual('Test 03');
+
+    // Update Article
+    const updateArticleResponse = await api
+        .path(`/articles/${slugId}`)
+        .headers({ Authorization: authToken })
+        .body({"article": {"title": "Test 03 Updated", "description": "Test Description 2", "body": "Test body", "tagList": []}})
+        .putRequest(200);
+    expect(updateArticleResponse.article.title).toEqual('Test 03 Updated');
+    const updatedSlugId = updateArticleResponse.article.slug;
+
+    // Verify Article Update
+    const updatedArticleResponse = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .params({limit:10, offset:0})
+        .getRequest(200)
+    expect(updatedArticleResponse.articles[0].title).toEqual('Test 03 Updated');
+
+    // Delete Article
+    await api
+        .path(`/articles/${updatedSlugId}`)
+        .headers({ Authorization: authToken })
+        .deleteRequest(204);
+
+    // Verify Article Deletion
+    const articlesResponseTwo = await api
+        .path('/articles')
+        .headers({ Authorization: authToken })
+        .params({limit:10, offset:0})
+        .getRequest(200);
+    expect(articlesResponseTwo.articles[0].title).not.toEqual('Test 03 Updated');
+})
